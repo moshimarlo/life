4672860bf4d158090d98fcde42ce3c6a92ad3a3c
Mikko Talikka <mikko.talikka@live.com>
2021-12-17 01:00:10 +1100

    Preprocess more things and refactor to remove redundant structures

--- src/main.rs
+++ src/main.rs
@@ -18,81 +18,73 @@ const SCREEN_SIZE: (f32, f32) = (
     GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
 );
 
-const DESIRED_FPS: u32 = 60;
-
-#[derive(Copy, Clone)]
-struct Cell {
-    x: u32,
-    y: u32,
-    alive: bool,
-    alive_next: bool,
-    rect: Rect,
-}
-
-impl Cell {
-    pub fn new() -> Self {
-        Self {
-            x: 0,
-            y: 0,
-            alive: false,
-            alive_next: false,
-            rect: Rect::new(
-                0. as f32 * GRID_CELL_SIZE.0 as f32,
-                0. as f32 * GRID_CELL_SIZE.1 as f32,
-                GRID_CELL_SIZE.0 as f32,
-                GRID_CELL_SIZE.1 as f32
-            ),
-        }
-    }
-
-    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
-        let color: Color = match self.alive {
-            true => ggez::graphics::Color::BLACK,
-            false => ggez::graphics::Color::WHITE,
-        };
-        let tile = Mesh::new_rectangle(
-            ctx,
-            DrawMode::fill(),
-            self.rect,
-            color
-        )?;
-        graphics::draw(ctx, &tile, (mint::Point2 {x: self.x as f32, y: self.y as f32},))
-    }
-}
+const DESIRED_FPS: u32 = 30;
 
 struct State {
-    board: [[Cell; GRID_SIZE.0]; GRID_SIZE.1],
+    board: Vec<u8>,
+    board_next: Vec<u8>,
+    meshes_alive: Vec<Mesh>,
+    meshes_dead: Vec<Mesh>,
+    rects: Vec<Rect>,
     paused: bool,
 }
 
 impl State {
-    pub fn new() -> Self {
+    pub fn new(ctx: &mut Context) -> Self {
         let mut s = Self {
-            board: [[Cell::new(); GRID_SIZE.0]; GRID_SIZE.1],
+            board: vec![],
+            board_next: vec![],
+            meshes_alive: vec![],
+            meshes_dead: vec![],
+            rects: vec![],
             paused: true,
         };
         let mut rng = rand::thread_rng();
-
+        let w = GRID_SIZE.0 as i32;
         for i in 0..GRID_SIZE.0 as usize {
             for j in 0..GRID_SIZE.1 as usize {
-                s.board[i][j].x = i as u32;
-                s.board[i][j].y = j as u32;
-                let alive = rng.gen::<bool>();
-                s.board[i][j].alive = alive;
-                s.board[i][j].alive_next = alive;
-                s.board[i][j].rect = Rect::new(
-                s.board[i][j].x as f32 * GRID_CELL_SIZE.0 as f32,
-                s.board[i][j].y as f32 * GRID_CELL_SIZE.1 as f32,
-                GRID_CELL_SIZE.0 as f32,
-                GRID_CELL_SIZE.1 as f32
-                );
+                s.rects.push(Rect::new(
+                    i as f32 * GRID_CELL_SIZE.0 as f32,
+                    j as f32 * GRID_CELL_SIZE.1 as f32,
+                    GRID_CELL_SIZE.0 as f32,
+                    GRID_CELL_SIZE.1 as f32
+                ));
+            }
+        }
+        for i in 0..GRID_SIZE.0 as usize {
+            for j in 0..GRID_SIZE.1 as usize {
+                let coords = (j as i32 * w + i as i32) as usize;
+                let one_or_zero = rng.gen::<bool>() as u8;
+                s.board.push(one_or_zero);
+                s.board_next.push(one_or_zero);
+                let mesh = match Mesh::new_rectangle(
+                    ctx,
+                    DrawMode::fill(),
+                    s.rects[coords],
+                    Color::BLACK
+                ) {
+                    Ok(mesh) => mesh,
+                    Err(_) => panic!("Error on creating mesh"),
+                };
+                s.meshes_alive.push(mesh);
+
+                let mesh = match Mesh::new_rectangle(
+                    ctx,
+                    DrawMode::fill(),
+                    s.rects[coords],
+                    Color::WHITE
+                ) {
+                    Ok(mesh) => mesh,
+                    Err(_) => panic!("Error on creating mesh"),
+                };
+                s.meshes_dead.push(mesh);
             }
         }
         s
     }
 
     fn toggle_cell(&mut self, x: usize, y: usize) {
-        self.board[x][y].alive = !self.board[x][y].alive;
+        self.board[y * GRID_SIZE.0 + x] ^= 0b0000_0001;
     } 
 
     fn check_neighbours(&mut self) {
@@ -100,50 +92,55 @@ impl State {
         let h = GRID_SIZE.1 as i32;
         for i in 0..GRID_SIZE.0 {
             for j in 0..GRID_SIZE.1 {
+                let coords = (j as i32 * w + i as i32) as usize;
                 let mut count = 0;
                 for x in i as i32 - 1..i as i32 + 2 {
                     for y in j as i32 - 1..j as i32 + 2 {
-                        if self.board[((x + w) % w) as usize][((y + h) % h) as usize].alive {
+                        if self.board[((((y + h) % h)) * w + ((x + w) % w)) as usize] == 1 {
                             count += 1;
                         }
                     }
                 }
-                if self.board[i][j].alive {
+                if self.board[coords] == 1 {
                     count -= 1;
                 }
-                match self.board[i][j].alive {
-                    true => {
-                        self.board[i][j].alive_next = true;
+                match self.board[coords] {
+                    1 => {
+                        self.board_next[coords] = 1;
                         if count < 2 || count > 3 {
-                            self.board[i][j].alive_next = false;
+                            self.board_next[coords] = 0;
                         }
-                    }
-                    false => {
-                        self.board[i][j].alive_next = false;
+                    },
+                    0 => {
+                        self.board_next[coords] = 0;
                         if count == 3 {
-                            self.board[i][j].alive_next = true;
+                            self.board_next[coords] = 1;
                         }
-                    }
+                    },
+                    _ => { panic!(); },
                 }
             }
         }
     }
 
     fn update_state(&mut self) {
+        let w = GRID_SIZE.0 as i32;
         for i in 0..GRID_SIZE.0 {
             for j in 0..GRID_SIZE.1 {
-                self.board[i][j].alive = self.board[i][j].alive_next;
+                let coords = (j as i32 * w + i as i32) as usize;
+                self.board[coords] = self.board_next[coords];
             }
         }
     }
 
     fn randomize(&mut self) {
         let mut rng = rand::thread_rng();
-        for i in 0..GRID_SIZE.0 as usize {
-            for j in 0..GRID_SIZE.1 as usize {
-                let alive = rng.gen::<bool>();
-                self.board[i][j].alive = alive;
-                self.board[i][j].alive_next = alive;
+        let w = GRID_SIZE.0 as i32;
+        for i in 0..GRID_SIZE.0 as i32 {
+            for j in 0..GRID_SIZE.1 as i32 {
+                let alive = rng.gen::<bool>() as u8;
+                self.board[(j * w + i) as usize] = alive;
+                self.board_next[(j * w + i) as usize] = alive;
             }
         }
     }
@@ -167,9 +164,26 @@ impl EventHandler<GameError> for State {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [1.0, 1.0, 1.0, 1.0].into());
+        let w = GRID_SIZE.0 as i32;
         for i in 0..GRID_SIZE.0 {
             for j in 0..GRID_SIZE.1 {
-                self.board[i][j].draw(ctx)?;
+                let coords = (j as i32 * w + i as i32) as usize;
+                match self.board[coords] {
+                    1 => {
+                        graphics::draw(
+                            ctx, 
+                            &self.meshes_alive[coords],
+                            (mint::Point2 {x: i as f32, y: j as f32},
+                        ))?;
+                    }
+                    0 => {
+                        graphics::draw(ctx,
+                            &self.meshes_dead[coords],
+                            (mint::Point2 {x: i as f32, y: j as f32},
+                        ))?;
+                    }
+                    _ => { panic!(); }
+                }
             }
         }
         graphics::present(ctx)?;
@@ -206,11 +220,12 @@ impl EventHandler<GameError> for State {
 }
 
 fn main() {
-    let state = State::new();
     let (ctx, event_loop) = ContextBuilder::new("life", "Mikko")
         .window_setup(conf::WindowSetup::default().title("Conway's Game of Life"))
         .window_mode(conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         .build()
         .unwrap();
+    let mut ctx = ctx;
+    let state = State::new(&mut ctx);
     event::run(ctx, event_loop, state);
 }
